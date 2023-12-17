06bb87a030e7ec144e9a72d8d5896354e23e017d
Eiji Kagimoto <22r301022ta@stu.teikyo-u.ac.jp>
2023-12-17 12:55:58 +0900

    vueプロジェクトの作成

---
+++ swgoh-vue-project/src/router/index.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import GeneralKenobi from '../views/GeneralKenobi.vue'
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes:[
+      {
+        path: '/',
+        name: 'GeneralKenobi',
+        component: GeneralKenobi
+      },
+    // {
+    //     paht: '/',
+    //     name: 'namae',
+    //     componet
+    // }
+    ]
+})
+export default router
